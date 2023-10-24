684f9d9115e035815e989048393b67d9319e3b4b
dominic <104272130+DominicBennett@users.noreply.github.com>
2023-10-24 21:51:58 +0100

    add theme button and script to change theme

--- scripts.js
+++ scripts.js
@@ -9,5 +9,34 @@ function logImages() {
 //returns the part of string after the supplied pattern
 const getImageName = (image) => image.slice(image.indexOf('o/')+1);
 
+var body = document.querySelector('body');
+var p = document.querySelector('p');
+var theme ='day';
+
+function changeTheme() {
+    if (theme === 'day') {
+        //set theme to night
+        body.style.backgroundColor = 'black';
+        for (var i = 0; i < paragraphs.length; i++) {
+            paragraphs[i].style.backgroundColor = 'black';
+            paragraphs[i].style.color = 'white';
+        }
+        theme = 'night';
+    } else {
+        //set theme to day
+        body.style.backgroundColor = 'white';
+        for (var i = 0; i < paragraphs.length; i++) {
+            paragraphs[i].style.backgroundColor = 'white';
+            paragraphs[i].style.color = 'black';
+        }
+        theme = 'day';
+    }
+}
+
+var themeButton = document.getElementById('themeButton');
+var paragraphs = document.querySelectorAll('p');
+
+themeButton.addEventListener('click', changeTheme);
+
 
 
