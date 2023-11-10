47b928b08e47dbdf0f491f5449eab4eba26cfef8
dominic <104272130+DominicBennett@users.noreply.github.com>
2023-11-10 21:09:31 +0000

    add jump to anchor to nav bar, fixed transparency of nav and footer elements background, footer no longer sticky

--- scripts.js
+++ scripts.js
@@ -11,6 +11,8 @@ const getImageName = (image) => image.slice(image.indexOf('o/')+1);
 
 var body = document.querySelector('body');
 var p = document.querySelector('p');
+var h2 = document.querySelector('h2');
+var pFooter =document.querySelector('.footer p');
 var theme ='day';
 
 function changeTheme() {
@@ -20,7 +22,13 @@ function changeTheme() {
         for (var i = 0; i < paragraphs.length; i++) {
             paragraphs[i].style.backgroundColor = 'black';
             paragraphs[i].style.color = 'white';
-        }
+        };
+        for (var i = 0; i < h2All.length; i++) {
+            h2All[i].style.backgroundColor = 'black';
+            h2All[i].style.color = 'white';
+        };
+        //pFooter.style.backgroundColor = 'transparent';
+
         theme = 'night';
     } else {
         //set theme to day
@@ -28,13 +36,18 @@ function changeTheme() {
         for (var i = 0; i < paragraphs.length; i++) {
             paragraphs[i].style.backgroundColor = 'white';
             paragraphs[i].style.color = 'black';
-        }
+        };
+        for (var i = 0; i < h2All.length; i++) {
+            h2All[i].style.backgroundColor = 'white';
+            h2All[i].style.color = 'black';
+        };
         theme = 'day';
     }
 }
 
 var themeButton = document.getElementById('themeButton');
 var paragraphs = document.querySelectorAll('p');
+var h2All = document.querySelectorAll('h2');
 
 themeButton.addEventListener('click', changeTheme);
 
